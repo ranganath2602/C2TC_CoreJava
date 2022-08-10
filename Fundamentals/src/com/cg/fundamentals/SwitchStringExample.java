52155eaffa8398553d0eb93903bd544592c06539
ranganath2602 <110548306+ranganath2602@users.noreply.github.com>
2022-08-10 21:21:36 +0530

    Add files via upload
    first commit

---
+++ Fundamentals/src/com/cg/fundamentals/SwitchStringExample.java
@@ -0,0 +1,22 @@
+public class SwitchStringExample {
+
+	public static void main(String[] args) {
+		String currentDay="Thursday";
+		
+		switch(currentDay) {
+		case "Monday":
+		case "Tuesday":
+		case "Wednesday":
+			System.out.println("boring day");
+		case "Thursday":
+			System.out.println("getting better");
+			break;
+		case"Friday":
+			System.out.println("much better");
+		case "Saturday":
+		case "Sunday":
+		
+		}
+	}
+
+}
---
+++ Fundamentals/src/com/cg/fundamentals/WhileExample.java
@@ -0,0 +1,13 @@
+
+public class WhileExample {
+
+	public static void main(String[] args) {
+		
+		int i =0;
+		while(true) {
+			System.out.print(i + " ");
+			i++;
+		}
+	}
+
+}
